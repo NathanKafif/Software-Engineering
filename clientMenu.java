418b94ac1f4a2cad67d8d1462b132fd45fa77237
NathanKafif <34772351+NathanKafif@users.noreply.github.com>
2017-12-22 19:55:50 +0200

    Update clientMenu.java

--- clientMenu.java
+++ clientMenu.java
@@ -1,23 +1,36 @@
 package protoType;
 
 import java.util.*;
+import java.text.SimpleDateFormat;
 
 public class clientMenu {
-	public static void main(){
+	public static void main(String[] args){
 		menu();
 	}
+	public clientMenu(){};
 	public static void menu(){
-		customerController controller;
-		customer me; //entity
-		clientLoggedIn logged; //next boundary/interface
+		//customerController controller;
+		Customer me; //entity
+		System.out.println("Hello guest");
 		System.out.println("Select one of the two options (write the digit and press enter):\n");
-		System.out.println("1 - Register \n2- Log in");
+		System.out.println("1 - Register \n2- Log in\n");
 		Scanner sc = new Scanner(System.in);
 		int input = sc.nextInt();
+		sc.nextLine();
 		switch (input){
 		case 1: //Register
 			System.out.println("Enter your name");
 			String name = sc.nextLine();
+			System.out.println("Enter your id");
+			int id = sc.nextInt();
+			sc.nextLine();
+			System.out.println("Enter your email");
+			String email = sc.nextLine();
+			System.out.println("Enter your car number");
+			int carNum = sc.nextInt();
+			sc.nextLine();
+			System.out.println("Enter your arrival date");
+			String arrivalDate = sc.nextLine();
 			System.out.println("Enter your arrival time");
 			String arrivalTime = sc.nextLine();
 			System.out.println("Enter your finishing time");
@@ -25,26 +38,29 @@ public class clientMenu {
 			System.out.println("Select one of the two parking lots (write the digit and press enter):\n");
 			System.out.println("1 - Kibbutz Yagur parking lot \n2- HaKarmel parking lot");
 			int parkingLotNum = sc.nextInt();
-			me = controller.addClient(name,arrivalTime,finishingTime, parkingLotNum);
+			sc.nextLine();
+			String dateOfRegistration = new SimpleDateFormat("dd-MM-yyyy").format(new Date());
+			me = controller.addCustomer(id, name, carNum,  finishingTime, email, parkingLotNum, dateOfRegistration, arrivalDate, arrivalTime);
 			if (me == null){
 				System.out.println("Registration failed. Try again");
 				menu();
 				break;
 			}
-			logged.menu(me);
+			me = new Customer(id, name, carNum,  finishingTime, email, parkingLotNum, 0, dateOfRegistration, arrivalDate, arrivalTime);
+			clientLoggedIn.menu(me);
 			break;
 		case 2: //Log in
 			System.out.println("Enter your client ID");
-			int id = sc.nextInt();
-			me = controller.findCustomer(id);
+			int ID = sc.nextInt();
+			sc.nextLine();
+			me = controller.findCustomer(ID);
 			if (me == null){
-				System.out.println("Wrong client ID. \n");
+				System.out.println("Wrong client ID");
 				menu();
-				break;
 			}
-			logged.menu(me);
+			clientLoggedIn.menu(me);
 			break;
 		}
-		
+		sc.close();
 	}
 }
